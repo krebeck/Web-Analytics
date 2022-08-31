7d47d9c032e1ea33e806fc7d152d2016116f2999
Aaron Krebeck <krebeck@wrlc.org>
2022-08-31 13:56:53 -0400

    Update ia.php

--- ia.php
+++ ia.php
@@ -16,43 +16,24 @@ if(!empty($_GET['ISSN'])){
 <html lang="en">
 <head>
   <meta charset="utf-0"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
   <title>Interactive Analytics</title>
-  <link rel="stylesheet" href="../Materialize/materialize.min.css"/>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-  <nav>
-        <div class="nav-wrapper blue"> 
-		<img src="../Materialize/wrlc-logo-white.png" height="50px" style="margin:5px 0 0 20px; position:absolute;"> 
-		<a href="#" class="brand-logo" style="margin-left:90px;">Web Analytics: Network and SCF reports... without Alma!</a>
-        </div>
-	<div class="position-absolute mx-auto help-button" style="top:2px;">
-            <a class="btn btn-info" href="home.html">HOME</a>
-        </div>
-  </nav>
-    <div class="jumbotron">
-      <div class="container task-AdminAddInstitution">
-        <div class="container mt-4 position-relative">
-        </div>
-        <div class="card">
-		  <div class="card-header">
-			<h2 class="card-title">Journal Volume Overlap Tool</h2>
-		  </div>
-		  <div class="card-body">
-		    <p><h3 style="font-size: 1em">Insert ISSN of title for accession to display all volumes of that title already in the SCF</h3></p>
-			<form action="">
-				<input type="text" name="ISSN" placeholder="1234-5678"/>
-				<button type="submit">Submit</button>
-			<br/>
-			</form>
-			<p>ISSN -- Title -- Barcode -- Tray Location -- <span style="font-weight:bold">Volume</span></p>
-			<?php
-			foreach ($analytics_xml->QueryResult->ResultXml->rowset->Row as $item ) {printf('<li>%s -- %s -- %s -- %s -- <span style="font-weight:bold">%s</span></li>', $item->Column1, $item->Column2, $item->Column4, $item->Column7, $item->Column6);}
-			?>
-	</div></div></div></div>
+<h1>Journal Volume Overlap Tool</h1>
+<h3>Insert ISSN of title for accession to display all volumes of that title already in the SCF</h3>
+<form action="">
+	<input type="text" name="ISSN"/>
+	<button type="submit">Submit</button>
+<br/>
+<pre>
+<?php
+	print_r($analytics_xml);
+?>
+</pre>
+</form>
+<p>ISSN -- Title -- Barcode -- Tray Location -- <span style="font-weight:bold">Volume</span></p>	
+<?php
+//**foreach ($analytics_xml->QueryResult->ResultXml->rowset->Row as $item ) {printf('<li>%s -- %s -- %s -- %s -- <span style="font-weight:bold">%s</span></li>', $item->Column1, $item->Column2, $item->Column4, $item->Column7, $item->Column6);}/*
+?>
 </body>
 </html>
